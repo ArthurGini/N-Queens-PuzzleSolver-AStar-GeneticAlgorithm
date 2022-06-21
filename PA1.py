65d7a2b042bec43d7d87dc3fce705da997d2dedd
ArthurGini <arthurbriganti@hotmail.com>
2022-06-20 21:40:33 -0300

    Admissivel pronto
    Co-authored-by: wagner2105 <wagner2105@users.noreply.github.com>
    Co-authored-by: Kevin Miguel <alukevin@users.noreply.github.com>

--- PA1.py
+++ PA1.py
@@ -2,7 +2,6 @@ from PyQt5 import QtWidgets as qtw, QtCore, QtGui
 from mainUI import Ui_MainWindow
 from PyQt5.QtCore import QThread, QTimer
 import sys, os, glob, PyQt5, time
-from algorithms.genetic_algorithm import RunGeneticAlgorithm
 from algorithms.a_star import RunAStarAlgorithm
 
 
@@ -15,7 +14,6 @@ if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
 
 
 folder = 'states_images'
-ga_output = []
 a_star_output = []
 
 
@@ -29,18 +27,9 @@ class MainWindow(qtw.QMainWindow):
         self.index = None
         print(self.index)
         self.image_names = []
-        
-        # Populating combobox values.
-        self.ui.elitismComboBox.addItem('Y')
-        self.ui.elitismComboBox.addItem('N')
-        self.ui.crossoverTypeComboBox.addItem('SP')
-        self.ui.crossoverTypeComboBox.addItem('TP')
-        self.ui.parentSelectComboBox.addItem('RS')
-        self.ui.parentSelectComboBox.addItem('RWS')
 
-        self.ga_thread = None
         self.a_star_thread = None
-        
+
         self.ui.autoTrace.clicked.connect(self.run_auto_trace)
         self.ui.stopAutotrace.clicked.connect(self.stop_auto_trace)
 
@@ -50,8 +39,7 @@ class MainWindow(qtw.QMainWindow):
         self.ui.runAStarButton.clicked.connect(self.run_a_star)
         self.ui.stopastarButton.clicked.connect(self.stop_a_star_thread)
 
-        self.ui.runGaButton.clicked.connect(self.run_genetic_algorithm)
-        self.ui.stopgaButton.clicked.connect(self.stop_ga_thread)
+
 
         self.ui.resetButton.clicked.connect(self.reset)
 
@@ -96,80 +84,37 @@ class MainWindow(qtw.QMainWindow):
         else:
             print('Output List is Empty!')
 
-    def run_genetic_algorithm(self):
-        """Function to run the Genetic algorithm. It uses a thread to run the algorithm in the background."""
-
-        N_queens = self.ui.nQueenSpinBox.value()
-        init_pop_size = self.ui.initpopSpinBox.value()
-        mutation_prob = self.ui.mutprobDoubleSpinBox.value()
-        max_gen = self.ui.maxgenSpinBox.value()
-        crossover_rate = self.ui.crossoverRateDoubleSpinBox.value()
-        elitism = self.ui.elitismComboBox.currentText()
-        crossover_method = self.ui.crossoverTypeComboBox.currentText()
-        parent_selection_algo= self.ui.parentSelectComboBox.currentText()
-
-        self.ga_thread = GeneticAlgorithmThread(
-                        N_queens=N_queens,
-                        init_pop_size=init_pop_size,
-                        mutation_prob=mutation_prob,
-                        max_gen=max_gen,
-                        crossover_method=crossover_method,
-                        crossover_rate=crossover_rate,
-                        parent_selection_algo=parent_selection_algo,
-                        elitism=elitism
-                        )
-
-        self.ui.outputLabel.setText("Genetic Algorithm Running...")
-        self.ga_thread.start()
-        self.ga_thread.finished.connect(self.show_ga_results)
-        
-    def stop_ga_thread(self):
-        """Function to stop the algorithm's thread and enable some buttons."""
-
-        self.ui.outputLabel.setText('Searching Stopped!')
-        self.ga_thread.terminate()
-
-        self.ui.nextButton.setDisabled(False)
-        self.ui.prevButton.setDisabled(False)
-        self.ui.autoTrace.setDisabled(False)
-        self.ui.stopAutotrace.setDisabled(False)
-        win.close() # Closing window as stopping on large inputs can sometimes freeze the window.
-
-    def show_ga_results(self):
-        """Function to display the output of the algorithm."""
-
-        global ga_output
-        if len(ga_output) != 0:
-            output = f'Steps = {ga_output[0]} | Time = {ga_output[1] :.4} secs | Solution Found = {ga_output[2]}'
-            self.ui.outputLabel.setText(output)
-            self.load_images_directory()
-        else:
-            print('Output List is Empty!')
-
     def run_auto_trace(self):
         """Function to run auto trace feature."""
 
         self.image_names.clear()
 
         self.load_images_directory()
-        
+
         self.file_it = iter(self.image_names)
-        
+
         self._timer = QtCore.QTimer(self, interval=300)
         self._timer.timeout.connect(self._on_timeout)
         self._timer.start()
-    
+
+
+    def stop_auto_trace(self):
+        """Function to stop the auto tracer."""
+
+        self._timer.stop()
+
+
     def _on_timeout(self):
         """Utility timeout function for auto tracing."""
 
         try:
             img = next(self.file_it)
-            
+
             self.ui.imagecountLabel.setText(img)
             self.ui.imageLabel.setPixmap(QtGui.QPixmap(folder+'\\'+img))
         except StopIteration:
             self._timer.stop()
-    
+
     def stop_auto_trace(self):
         """Function to stop the auto tracer."""
 
@@ -186,28 +131,28 @@ class MainWindow(qtw.QMainWindow):
         self.ui.prevButton.setDisabled(False)
         self.ui.autoTrace.setDisabled(False)
         self.ui.stopAutotrace.setDisabled(False)
-            
+
     def show_next_image(self):
         """Function to show next image in line."""
-        
+
         if self.index is None:
             self.index = 0
 
         img = self.image_names[self.index]
         self.ui.imageLabel.setPixmap(QtGui.QPixmap(folder+'\\'+img))
         self.ui.imagecountLabel.setText(img)
-        self.index += 1 
+        self.index += 1
         self.index = self.index % len(self.image_names)
-        
+
     def show_prev_image(self):
         """Function to show previous image in line."""
 
-        self.index -= 1 
+        self.index -= 1
         self.index = self.index % len(self.image_names) # Using the property og -ve modulo in python.
         img = self.image_names[self.index]
         self.ui.imageLabel.setPixmap(QtGui.QPixmap(folder+'\\'+img))
-        self.ui.imagecountLabel.setText(img)   
-    
+        self.ui.imagecountLabel.setText(img)
+
     def reset(self):
         self.ui.outputLabel.setText("Steps/Time")
         self._clear_states_images_folder()
@@ -235,20 +180,7 @@ class MainWindow(qtw.QMainWindow):
         """Function to display initial instructions again."""
 
         _translate = QtCore.QCoreApplication.translate
-        text = '''
-        <html><head/><body><p align="justify"><span style=" font-size:12pt; font-weight:600; text-decoration: underline;">Instructions</span>
-        <span style=" font-size:12pt; font-weight:600;">:</span>
-        </p><p align="justify">1. Choose to run A* or Genetic algorithm at a time.</p>
-        <p align="justify">2. When output appears on the top label, then click &lt;Next&gt; button </p>
-        <p align="justify">to view the images (for visual tracing) or use &lt;Auto Trace&gt;.</p>
-        <p align="justify">NOTE: Check if the folder &lt;states_images&gt; is created or not.</p>
-        <p align="justify">3. Click on &lt;RESET&gt; button to clear &lt;states_images&gt; folder.</p>
-        <p align="justify">Then run any algorithm again.</p>
-        <p align="justify"><span style=" font-weight:600; text-decoration: underline;">Dictionary:</span></p>
-        <p align="justify">1. Crossover type - SP (Single point), TP (Two point)</p>
-        <p align="justify">2. Parent selection - RS (Rank selection), RWS (Roulette wheel selection)</p>
-        <p align="justify">3. Elitism - Y (Yes), N (No)</p><p align="justify">ENJOY THE APP!</p></body></html>
-        '''
+        text = '''<html><head /><body>	<p align=\"justify\"><span style=\" font-size:16pt; font-weight:600; text-decoration:			underline;\">Instrucoes:</span><span style=\" font-size:12pt; font-weight:600;\">:</span></p>	<p align=\"justify\"><span style=\" font-size:12pt; font-weight:300; text-decoration: bold;\"></span>1. Escolha a quantidade de rainhas n.</p></body></html>'''
         self.ui.imageLabel.setText(_translate("MainWindow", text))
 
 
@@ -262,34 +194,6 @@ class AStarAlgorithmThread(QThread):
     def run(self):
         global a_star_output
         a_star_output = RunAStarAlgorithm.run_a_star(N_queens=self.N_queens)
-
-
-class GeneticAlgorithmThread(QThread):
-    """Class that runs the algorithm as a thread."""
-
-    def __init__(self, N_queens, init_pop_size, mutation_prob, max_gen, crossover_method, crossover_rate, parent_selection_algo, elitism):
-        super().__init__()
-        self.N_queens = N_queens
-        self.init_pop_size = init_pop_size
-        self.mutation_prob = mutation_prob
-        self.max_gen = max_gen
-        self.crossover_method = crossover_method
-        self.crossover_rate = crossover_rate
-        self.parent_selection_algo = parent_selection_algo
-        self.elitism = elitism
-
-    def run(self):
-        global ga_output
-        ga_output = RunGeneticAlgorithm.run_ga(
-                    N_queens=self.N_queens,
-                    init_pop_size=self.init_pop_size,
-                    mutation_prob=self.mutation_prob,
-                    max_gen=self.max_gen,
-                    crossover_rate=self.crossover_rate,
-                    crossover_method=self.crossover_method,
-                    parent_selection_algo=self.parent_selection_algo,
-                    elitism=self.elitism
-                    )
 
 
 
---
+++ classes.md
@@ -0,0 +1,53 @@
+<!-- FIXME: Levar pro outro repo e subir no git  -->
+
+
+# Arquivos:
+
+**Kevin e Sorocaba**
+## A_star:
+### Classe:
+1. State(params)
+#### Funções:
+
+### Classe:
+2. AStarAlgorithm(params)
+#### Funções:
+- run
+Função que executa o RunAStarAlgorithm
+### Classe:
+3. RunAStarAlgorithm(params)
+#### Funções:
+
+**Arthur Gini aqui**
+## PA1:
+### Classe:
+1. MainWindow(params)
+#### Funções:
+- run_a_star(params)
+- stop_a_star_thread
+- show_a_star_results
+### Classe:
+2. AStarAlgorithmThread(params)
+#### Funções:
+- run_a_star(params)
+- stop_a_star_thread
+- show_a_star_results
+
+**Arthur Gini aqui**
+## MainUI:
+### Classe:
+1. Ui_MainWindow(params)
+#### Funções:
+- setupUi
+- retranslateUi
+
+
+Para a heuristica não admissivel colocar uma quantidade de rainhas maior que
+o tamanho do tabuleiro
+
+5 rainhas para um tabuleiro 4x4
+
+Colocar uma mensagem de erro na tela do usuario falando que a heuristica é impossivel
+
+
+Para isso add mais um campo de seleção
